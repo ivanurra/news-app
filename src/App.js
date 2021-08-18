2b3e8640c98dc09e031ab93814a8134033c0355d
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-18 16:09:15 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Sky is the limit</div>;
+  return (
+    <Fragment>
+      <Header titulo="NSE - News Search Engine" />
+      <div className="container white">
+        <h1>Open your doors!</h1>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
